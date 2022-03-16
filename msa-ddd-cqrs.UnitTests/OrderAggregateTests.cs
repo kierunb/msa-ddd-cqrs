1f36648a414e0e9d0dc39c52bbbbe5ad6f967f58
kierunb <kierunb@gmail.com>
2022-03-16 14:01:19 +0100

    refactoring to rich model - part 1, migrations added

--- msa-ddd-cqrs.UnitTests/OrderAggregateTests.cs
+++ msa-ddd-cqrs.UnitTests/OrderAggregateTests.cs
@@ -1,15 +1,25 @@
 using msa_ddd_cqrs.Domain.Aggregates.Order;
+using System;
 using Xunit;
 
 namespace msa_ddd_cqrs.UnitTests;
 
 public class OrderAggregateTests
 {
+    //[Fact]
+    //public void AnemicModelUsageTest()
+    //{
+    //    var order = new Order { Id = 1, Address = "Address 1", IsDraft = true };
+    //    order.OrderItems.Add(new OrderItem { Name = "Item 1", Price = 12.34M });
+
+    //    Assert.True(order is not null);
+    //}
+
     [Fact]
-    public void AnemicModelUsageTest()
+    public void RichModelUsageTest1()
     {
-        var order = new Order { Id = 1, Address = "Address 1", IsDraft = true };
-        order.OrderItems.Add(new OrderItem { Name = "Item 1", Price = 12.34M });
+        var order = new Order(buyerId: Guid.NewGuid(), address: "Address 1", orderStatus: "Created", isDraft: false );
+        order.AddOrderItem(name: "Product 1", price: 12.34M, units: 3);
 
         Assert.True(order is not null);
     }
--- msa-ddd-cqrs/Domain/Aggregates/Order/Order.cs
+++ msa-ddd-cqrs/Domain/Aggregates/Order/Order.cs
@@ -1,13 +1,33 @@
-﻿namespace msa_ddd_cqrs.Domain.Aggregates.Order;
+﻿using msa_ddd_cqrs.Domain.Shared;
 
-public class Order
+namespace msa_ddd_cqrs.Domain.Aggregates.Order;
+
+public class Order : Entity, IAggregateRoot
 {
-    public int Id { get; set; }
-    public Guid BuyerId { get; set; }
-    public string Address { get; set; }
-    public string OrderStatus { get; set; }
-    public bool IsDraft { get; set; }
+    // fields could be also private for better encapsulation
+    // 'Id' inhreited from Entity base class
+
+    public Guid BuyerId { get; private set; }
+    public string Address { get; private set; }
+    public string OrderStatus { get; private set; }
+    public bool IsDraft { get; private set; }
 
 
-    public List<OrderItem> OrderItems { get; set; } = new();
+    public List<OrderItem> OrderItems { get; private set; } = new();
+
+    protected Order() { }
+
+    public Order(Guid buyerId, string address, string orderStatus, bool isDraft)
+    {
+        BuyerId = buyerId;
+        Address = address;
+        OrderStatus = orderStatus;
+        IsDraft = isDraft;
+    }
+
+    public void AddOrderItem(string name, decimal price, int units)
+    {
+        var orderItem = new OrderItem(name, price, units);
+        OrderItems.Add(orderItem);
+    }
 }
